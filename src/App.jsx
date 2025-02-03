fcdfd4d33eae830f3e365b22c566db86e19869a8
vanjasalamon <vanjasalamon9@gmail.com>
2025-02-03 10:55:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <>
+     <h1 class="text-3xl font-bold underline">
+      Pracenje navika    
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    solidPlugin()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
+});
